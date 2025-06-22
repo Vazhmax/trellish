4f0e8c83a678aa56fd2ef0632c0cd1543a7e45de
Vazhmax <Vazhmax0204@gmail.com>
2025-06-22 19:34:20 +0500

    Initial commit

---
+++ src/test/java/org/vazhmax/trellish/TrellishApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vazhmax.trellish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrellishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
